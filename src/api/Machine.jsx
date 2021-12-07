aae327cac56205a06af93f79cc9580b074d94656
canmert <canmert.dev@gmail.com>
2021-12-07 17:37:51 +0300

    changing machines api

--- src/api/Machine.jsx
+++ src/api/Machine.jsx
@@ -1,7 +1,5 @@
 /* eslint-disable import/prefer-default-export */
 import axios from 'axios';
 
-// const BaseURL = 'http://192.168.0.101:5000/api/machines';
-// const BaseURL = process.env.REACT_APP_API_PRODUCTION;
-const BaseURL = 'https://coffeetime.cf/api/machines';
+const BaseURL = 'https://coffeetime.link/api/machines';
 export const fetchMachinesByUserLocation = (lat, long) => axios.get(`${BaseURL}/mylocation/${lat}/${long}`);
