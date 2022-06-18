6829fed722b3a5413e20032e9e65b5f55b5856b7
canmert <canmert.dev@gmail.com>
2022-06-19 01:34:26 +0300

    adding bottom modal

--- src/screens/MapScreen/Map/index.jsx
+++ src/screens/MapScreen/Map/index.jsx
@@ -1,28 +1,48 @@
 /* eslint-disable react/forbid-prop-types */
-import React from 'react';
+import React, { createRef, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet } from 'react-native';
 import MapView from 'react-native-maps';
+import { BottomSheetModal } from '@gorhom/bottom-sheet';
 import MarkerList from '../MarkerList';
+import MarkerModal from '../MarkerModal';
 
-const Map = ({ machines, initialRegion }) => (
-  <>
-    <MapView
-      style={styles.map}
-      initialRegion={initialRegion}
-      showsMyLocationButton
-      showsUserLocation
-      zoomEnabled
-      loadingEnabled
-      mapType="standard"
-      loadingBackgroundColor="#e9dcbe"
-      loadingIndicatorColor="black"
-      moveOnMarkerPress={false}
-    >
-      <MarkerList machines={machines} />
-    </MapView>
-  </>
-);
+const Map = ({ machines, initialRegion }) => {
+  const bottomSheetRef = createRef(BottomSheetModal);
+  const [selectedMachine, setSelectedMachine] = useState(undefined);
+  const onMachineIconPress = (machine) => {
+    setSelectedMachine(machine);
+    bottomSheetRef.current.present();
+    console.log('icon pressed');
+  };
+
+  const onMapPress = () => {
+    bottomSheetRef.current.close();
+  };
+  return (
+    <>
+      <MapView
+        style={styles.map}
+         // provider={PROVIDER_GOOGLE}
+        initialRegion={initialRegion}
+        showsMyLocationButton
+        showsUserLocation
+        zoomEnabled
+        loadingEnabled
+        mapType="standard"
+        loadingBackgroundColor="#e9dcbe"
+        loadingIndicatorColor="black"
+        moveOnMarkerPress
+        onPress={() => onMapPress()}
+         // region={region}
+           // onMapReady={(e) => console.log(e)}
+      >
+        <MarkerList machines={machines} onPress={(machine) => onMachineIconPress(machine)} />
+      </MapView>
+      <MarkerModal modalRef={bottomSheetRef} machine={selectedMachine} />
+    </>
+  );
+};
 const styles = StyleSheet.create({
   map: {
     ...StyleSheet.absoluteFillObject,
